b54a1aca00a29ff5ca327c7405021eed04161a6e
szalaibalint <szalaibalint7521@gmail.com>
2024-04-26 08:30:07 +0200

    14

--- placeorder.php
+++ placeorder.php
@@ -1,11 +1,107 @@
 <?php
-unset($_SESSION['cart']);
+//unset($_SESSION['cart']);
 ?>
-<?=template_header('Place Order')?>
+<?=template_header('Products')?>
 
-<div class="placeorder content-wrapper">
-    <h1>Your Order Has Been Placed</h1>
-    <p>Thank you for ordering with us! We'll contact you by email with your order details.</p>
-</div>
+<head>
+    <style>
+            input::-webkit-outer-spin-button,
+            input::-webkit-inner-spin-button {
+                -webkit-appearance: none;
+                margin: 0;
+            }
 
-<?=template_footer()?>
+            input[type="number"] {
+                -moz-appearance: textfield;
+            }
+        </style>
+</head>
+
+<!-- Hero Section Begin -->
+<section class="hero hero-normal">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3">
+                    <div class="hero__categories">
+                        <div class="hero__categories__all">
+                            <i class="fa fa-bars"></i>
+                            <span>Kategóriák</span>
+                        </div>
+                        <ul>
+                            <li><a href="index.php?page=products&cat=1">Sport</a></li>
+                            <li><a href="index.php?page=products&cat=2">Fiatalsági irodalom</a></li>
+                            <li><a href="index.php?page=products&cat=3">Fikció</a></li>
+                            <li><a href="index.php?page=products&cat=4">Irodalom</a></li>
+                            <li><a href="index.php?page=products&cat=5">Filozófia</a></li>
+                            <li><a href="index.php?page=products&cat=6">Naplók</a></li>
+                            <li><a href="index.php?page=products&cat=7">Krimi</a></li>
+                            <li><a href="index.php?page=products&cat=8">Akció</a></li>
+                            <li><a href="index.php?page=products&cat=9">Thriller</a></li>
+                            
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-9">
+                    <div class="hero__search">
+                        <div class="hero__search__form">
+                            <form action="#">
+                                <input type="text" placeholder="Mit szeretnél keresni?">
+                                <button type="submit" class="site-btn">Keresés</button>
+                            </form>
+                        </div>
+                        <div class="hero__search__phone">
+                            <div class="hero__search__phone__icon">
+                                <i class="fa fa-phone"></i>
+                            </div>
+                            <div class="hero__search__phone__text">
+                                <h5>+36 70 425 9419</h5>
+                                <span>Ügyfélszolgálat 0-24</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Hero Section End -->
+
+
+    <!-- Breadcrumb Section Begin -->
+    <section class="breadcrumb-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <div class="breadcrumb__text">
+                        <h2 style="color: black;">Thanks for purchase big man</h2>
+                        <h5>Big thanks big man</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Breadcrumb Section End -->
+
+
+    <!-- Shoping Cart Section Begin -->
+    <section class="shoping-cart spad">
+        <div class="container">
+            
+        </div>
+    </section>
+    <!-- Shoping Cart Section End -->
+
+
+    <?=template_footer()?>
+
+
+
+    <!-- Js Plugins -->
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.nice-select.min.js"></script>
+    <script src="js/jquery-ui.min.js"></script>
+    <script src="js/jquery.slicknav.js"></script>
+    <script src="js/mixitup.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/main.js"></script>
+
